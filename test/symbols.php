47d6e53a95aa1bbc02fd334b97f13a1d7efa06a7
WesNetmo <netmosfera@gmail.com>
2017-02-01 07:20:06 +0100

    Fixed tests.

--- test/symbols.php
+++ test/symbols.php
@@ -9,8 +9,8 @@ const EC = ClassMemberAccessError::CLASS;
 
 function errMsg($className, $memberName, $context = ''){
     if($context === ''){
-        return "Referenced the either undefined or non-public object member `$className::$memberName`.";
+        return "Referenced the either undefined or non-public object member `$className::$memberName`";
     }else{
-        return "Referenced the either undefined or non-public object member `$className::$memberName` from context `$context`.";
+        return "Referenced the either undefined or non-public object member `$className::$memberName` from context `$context`";
     }
 }
